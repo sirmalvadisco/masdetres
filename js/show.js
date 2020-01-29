8705699599c69ae46f6046971f58442142db526b
sirmalvadisco <nikygost13@hotmail.com>
2020-01-29 14:57:18 -0300

    Version 1.1.6
    Changelog:
    
    -Fixed bug with black mode

--- js/show.js
+++ js/show.js
@@ -9,17 +9,17 @@ $('a.active-chords').click(function() {
 
 
 
-    $("#US_toggle").click(function(){      
+ $("#US_toggle").click(function(){      
       if($("#US_toggle .ui-flipswitch").hasClass('ui-flipswitch-active')){  
 		$("div.ui-content").addClass('black-mode');
-		$("div.ui-page-theme-b").children().removeClass('black-mode');
-        $("div.ui-page-theme-b").children().addClass('red-mode');
+		$("div.conquistadores").children().removeClass('black-mode');
+        $("div.conquistadores").children().addClass('red-mode');
 		$("a.list, a.listcqs").addClass('list-black-mode');
 		$("p, span.chord").addClass('letter-black-mode');
 		$("a.btn-back-fixed").addClass('btn-black-mode ui-alt-icon');  
       }else{
         $("div.ui-content").removeClass('black-mode');
-		$("div.ui-page-theme-b").children().removeClass('red-mode');
+		$("div.conquistadores").children().removeClass('red-mode');
 		$("a.list, a.listcqs").removeClass('list-black-mode');
 		$("p, span.chord").removeClass('letter-black-mode');
 		$("a.btn-back-fixed").removeClass('btn-black-mode ui-alt-icon'); 
